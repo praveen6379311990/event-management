8bb91543c457bbd6110da1322f88e13d3c249c69
Praveen <veradispraveen@gmail.com>
2025-05-26 23:57:15 +0530

    second stage

--- database/migrations/2025_05_26_171712_create_personal_access_tokens_table.php
+++ database/migrations/2025_05_26_171712_create_personal_access_tokens_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
+            $table->morphs('tokenable');
             $table->string('name');
-            $table->text('description');
-            $table->dateTime('start_date');
-            $table->dateTime('end_date');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('personal_access_tokens');
     }
 };
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\api\AttendeeController;
+use App\Http\Controllers\api\EventController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('events',EventController::class);
+
+Route::apiResource('events.attendees',AttendeeController::class)->scoped(['attendees' =>'events']);
